9d41b360bc5796391ebfe544b288c0bfd393c2a6
josejunquera <jose.junquera@hotmail.com>
2020-11-18 13:00:13 +0100

    Added ejercicio número primo

--- starter-project/www/js/src.js
+++ starter-project/www/js/src.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const numero = 11;
+let esPrimo = true;
+
+// Empezamos directamente en "2" porque todos los números son divisibles entre "1". Del mismo modo omitimos
+// al propio número (el 7) con el "menor que" en la condición.
+for (let i = 2; i < numero; i++) {
+  // Este "console.log" es para ayudaros a ver qué pasa en cada repetición. La sintaxis puede resultar
+  // un tanto extraña pero no te preocupes, no tienes por qué entender esto a estas alturas.
+  console.log(`En esta repetición compruebo el resto de ${numero} / ${i}.`);
+  console.log(numero % i);
+
+  // Comprobamos si el resto es igual a 0. Si lo es, cambiamos la variable "esPrimo" a "false".
+  if (numero % i === 0) {
+    esPrimo = false;
+  }
+}
+
+if (esPrimo) {
+  console.log(numero);
+  console.log('Es primo.');
+} else {
+  console.log(numero);
+  console.log('No es primo.');
+}
