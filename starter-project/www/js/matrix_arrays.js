f68daf65e80748b287e5c1b5da73824898de99df
josejunquera <jose.junquera@hotmail.com>
2020-11-24 18:24:22 +0100

    Matrix exercise finished

--- starter-project/www/js/matrix_arrays.js
+++ starter-project/www/js/matrix_arrays.js
@@ -6,16 +6,59 @@ const matrix = [
   ['*', '*', '*'],
 ];
 
-console.log(matrix);
+// matrix[1][2] = 'X';
+// console.log(matrix);
 
-/*Primer array secundario siempre primera posición simbolo
-  Cada linea siguiente +1 en la posición para insertar simbolo.
+//FUNCIÓN INSERTAR SIMBOLOS PRIMERA DIAGONAL
 
-*/
+// function insertSymbol(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j <= i; j++) {
+//       array[i][i] = 'X';
+//     }
+//   }
+//   return array;
+// }
+// console.log(insertSymbol(matrix));
+
+//FUNCIÓN INSERTAR SIMBOLOS SEGUNDA DIAGONAL
+
+// function insertSymbol(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j <= i; j++) {
+//       array[i][array.length - i - 1] = 'X';
+//     }
+//   }
+//   return array;
+// }
+// console.log(insertSymbol(matrix));
+
+//CREAR UNA MATRIX DE X*Y
+
+function createSquareMAtrix(filasColumnas) {
+  let matrix = [];
+  for (let i = 0; i < filasColumnas; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < filasColumnas; j++) {
+      matrix[i][j] = '*';
+    }
+  }
+  return matrix;
+}
+
+// FUNCIÓN INSERTAR SIMBOLOS EN AMBAS DIAGONALES
 
 function insertSymbol(array) {
   for (let i = 0; i < array.length; i++) {
-    // console.log(i);
+    for (let j = 0; j <= i; j++) {
+      array[i][i] = 'X';
+    }
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j <= i; j++) {
+        array[i][array.length - i - 1] = 'X';
+      }
+    }
   }
+  return array;
 }
-insertSymbol(matrix);
+console.log(insertSymbol(createSquareMAtrix(7)));
