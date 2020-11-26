0b6dee6dc24035c701fd815a77b22a35dde8f83e
josejunquera <jose.junquera@hotmail.com>
2020-11-26 16:28:11 +0100

    New sort algorithm added

--- starter-project/www/js/recursividad.js
+++ starter-project/www/js/recursividad.js
@@ -16,6 +16,7 @@ function factorial(n) {
 console.log(factorial(4));
 
 // FUNCIÓN PARA CALCULAR FIBONACCI
+
 // Serie fibonacci 0 1 1 2 3 5 8 13 21 34 55 89 144 => se suman siembre los dos números anteriores para calcular el siguiente
 
 function fibonacci(x) {
@@ -25,4 +26,19 @@ function fibonacci(x) {
     return fibonacci(x - 1) + fibonacci(x - 2);
   }
 }
-console.log(fibonacci(8));
+console.log(fibonacci(40));
+
+// FIBONACCI CON CACHE .
+
+const cache = { 0: 0, 1: 1 };
+
+function fibonacciMemo(x) {
+  if (cache[x] || x === 0) {
+    return cache[x];
+  }
+  const computedValue = fibonacciMemo(x - 1) + fibonacciMemo(x - 2);
+  cache[x] = computedValue;
+  return computedValue;
+}
+
+console.log(fibonacciMemo(40));
--- starter-project/www/js/sort_algorithm.js
+++ starter-project/www/js/sort_algorithm.js
@@ -1,16 +1,50 @@
 'use strict';
 
-let array = [55, 74, 87, 42, 19, 12, 25, 33, 26];
+let arrayUnsorted = [55, 74, 87, 42, 19, 12, 25, 33, 26];
 
-for (let i = 0; i < array.length; i++) {
-  //   console.log(i);
-  for (let j = 0; j < array.length; j++) {
-    // console.log(j);
-    if (array[j] > array[j + 1]) {
-      let aux = array[j];
-      array[j] = array[j + 1];
-      array[j + 1] = aux;
+// FUNCIÓN SORT
+
+// array.sort((a, b) => {
+//   return a - b;
+// });
+// console.log(array);
+
+// BUBBLE SORT
+
+// function sortBubble(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     //   console.log(i);
+//     for (let j = 0; j < array.length; j++) {
+//       // console.log(j);
+//       if (array[j] > array[j + 1]) {
+//         let aux = array[j];
+//         array[j] = array[j + 1];
+//         array[j + 1] = aux;
+//       }
+//     }
+//   }
+//   return array;
+// }
+// console.log(sortBubble(array));
+
+// INSERTION SORT
+
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(i);
+    console.log(array[i]);
+    let value = array[i];
+    let index = i;
+    while (index > 0 && array[index - 1] > value) {
+      array[index] = array[index - 1];
+      index = index - 1;
+      array[index] = value;
     }
   }
-  console.log(array);
+  return array;
 }
+console.log(insertionSort(arrayUnsorted));
+
+// IDEA ALGORITMO
+// crear una copia e insertar solo el menor
+// insertar randoms solo si son mayores que el que ya hay...
